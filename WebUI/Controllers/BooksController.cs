2916c75ba9a69226a57514fb2941817bf757697d
VasylSalabay <turboguy@ukr.net>
2019-03-11 23:50:25 +0200

    myfixesss

--- WebUI/Controllers/BooksController.cs
+++ WebUI/Controllers/BooksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Domain.Abstract;
+using Domain.Entities;
 
 namespace WebUI.Controllers
 {
@@ -16,9 +17,19 @@ namespace WebUI.Controllers
         {
             repository = repo;
         }
-
-        public ViewResult List(string genre, int page = 1)
+        public BooksController():base()
         {
+
+        }
+
+        public ViewResult List(string genre="", int page = 1)
+        {
+            List<Book> kek = new List<Book>
+                {
+                new Book { Name="Mova programs c# i platforma .Net 4.5", Author = "Troeslen e.t", Price = 1123},
+                new Book { Name = "Mova programs c++ i platforma .Net 4.5", Author = "oeslen e.t", Price = 2323 },
+                new Book { Name = "Mova programs c i platforma .Net 4.5", Author = "Troesn e.t", Price = 14523 },
+            };
 
             //    BooksListViewModel model = new BooksListViewModel
             //    {
@@ -39,7 +50,8 @@ namespace WebUI.Controllers
             //    };
 
             //    return View(model);
-            return View(repository.Books);
+            //  return View(repository.Books);
+            return View(kek);
         }
     }
 }
