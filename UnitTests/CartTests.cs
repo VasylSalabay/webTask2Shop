4590c735813daa4fc3d41c6c68850f4e5ea3a1f7
VasylSalabay <turboguy@ukr.net>
2019-03-16 00:00:48 +0200

    ADD CART

---
+++ UnitTests/CartTests.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Abstract;
+using Moq;
+using WebUI.Controllers;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            // Організація
+            Book book1 = new Book { BookId = 1, Name = "Book1" };
+            Book book2 = new Book { BookId = 2, Name = "Book2" };
+
+            Cart cart = new Cart();
+
+            // Дія
+            cart.AddItem(book1, 1);
+            cart.AddItem(book2, 1);
+            List<CartLine> results = cart.Lines.ToList();
+
+            // Ствердження
+            Assert.AreEqual(results.Count(), 2);
+            Assert.AreEqual(results[0].Book, book1);
+            Assert.AreEqual(results[1].Book, book2);
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            // Організація
+            Book book1 = new Book { BookId = 1, Name = "Book1" };
+            Book book2 = new Book { BookId = 2, Name = "Book2" };
+
+            Cart cart = new Cart();
+
+            // Дія
+            cart.AddItem(book1, 1);
+            cart.AddItem(book2, 1);
+            cart.AddItem(book1, 5);
+            List<CartLine> results = cart.Lines.OrderBy(c => c.Book.BookId).ToList();
+
+            // Ствердження
+            Assert.AreEqual(results.Count(), 2);
+            Assert.AreEqual(results[0].Quantity, 6);
+            Assert.AreEqual(results[1].Quantity, 1);
+        }
+
+        [TestMethod]
+        public void Can_Remove_Line()
+        {
+            // Організація
+            Book book1 = new Book { BookId = 1, Name = "Book1" };
+            Book book2 = new Book { BookId = 2, Name = "Book2" };
+            Book book3 = new Book { BookId = 3, Name = "Book3" };
+
+            Cart cart = new Cart();
+
+            // Дія
+            cart.AddItem(book1, 1);
+            cart.AddItem(book2, 1);
+            cart.AddItem(book1, 5);
+            cart.AddItem(book3, 2);
+            cart.RemoveLine(book2);
+
+            // Ствердження
+            Assert.AreEqual(cart.Lines.Where(c => c.Book == book2).Count(), 0);
+            Assert.AreEqual(cart.Lines.Count(), 2);
+        }
+
+        [TestMethod]
+        public void Calculate_Cart_Total()
+        {
+            // Організація
+            Book book1 = new Book { BookId = 1, Name = "Book1", Price = 100 };
+            Book book2 = new Book { BookId = 2, Name = "Book2", Price = 55 };
+
+            Cart cart = new Cart();
+
+            // Дія
+            cart.AddItem(book1, 1);
+            cart.AddItem(book2, 1);
+            cart.AddItem(book1, 5);
+            decimal result = cart.ComputeTotalValue();
+
+            // Ствердження
+            Assert.AreEqual(result, 655);
+        }
+
+        [TestMethod]
+        public void Can_Clear_Contents()
+        {
+            // Організація
+            Book book1 = new Book { BookId = 1, Name = "Book1", Price = 100 };
+            Book book2 = new Book { BookId = 2, Name = "Book2", Price = 55 };
+
+            Cart cart = new Cart();
+
+            // Дія
+            cart.AddItem(book1, 1);
+            cart.AddItem(book2, 1);
+            cart.AddItem(book1, 5);
+            cart.Clear();
+
+            // Ствердження
+            Assert.AreEqual(cart.Lines.Count(), 0);
+        }
+
+
+        // Добавлення елемента в кошик
+        [TestMethod]
+        public void Can_Add_To_Cart()
+        {
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>{
+                new Book {BookId = 1, Name = "Book1", Genre = "Genre1"}
+            }.AsQueryable());
+
+            Cart cart = new Cart();
+
+            CartController controller = new CartController(mock.Object);
+
+            controller.AddToCart(cart, 1, null);
+
+            Assert.AreEqual(cart.Lines.Count(), 1);
+            Assert.AreEqual(cart.Lines.ToList()[0].Book.BookId, 1);
+        }
+
+        // Після добавлення книги в корзину - перенаправлення на сторінку корзини
+        [TestMethod]
+        public void Adding_Book_To_Cart_Goes_To_Cart_Screen()
+        {
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>{
+                new Book {BookId = 1, Name = "Book1", Genre = "Genre1"}
+            }.AsQueryable());
+
+            Cart cart = new Cart();
+
+            CartController controller = new CartController(mock.Object);
+
+            RedirectToRouteResult result = controller.AddToCart(cart, 2, "myUrl");
+
+            Assert.AreEqual(result.RouteValues["action"], "Index");
+            Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+        }
+
+        [TestMethod]
+        public void Can_View_Cart_Contents()
+        {
+            Cart cart = new Cart();
+            CartController target = new CartController(null);
+
+            CartIndexViewModel result = (CartIndexViewModel)target.Index(cart, "myUrl").ViewData.Model;
+
+            Assert.AreSame(result.Cart, cart);
+            Assert.AreEqual(result.ReturnUrl, "myUrl");
+        }
+    }
+}
